62ceb301b91a04e9ff631abfb739a73e69b29263
dzucconi <damonzucconi@gmail.com>
2013-06-24 12:56:00 -0400

    Use native color pickers to implement updating

--- application.js
+++ application.js
@@ -12,46 +12,73 @@ $(function() {
 
   two = new Two(stage.params).appendTo(stage.el);
 
-  var Drawing = {
-    base: {
-      _0: two.makePolygon(5,432, 72,499, 72,365),
-      _1: two.makePolygon(859,432, 792,365, 792,499),
-      _2: two.makePolygon(432,859, 499,792, 365,792),
-      _3: two.makePolygon(432,5, 365,72, 499,72)
+  window.Drawing = {
+    groups: {
+      base: {
+        _0: two.makePolygon(5,432, 72,499, 72,365),
+        _1: two.makePolygon(859,432, 792,365, 792,499),
+        _2: two.makePolygon(432,859, 499,792, 365,792),
+        _3: two.makePolygon(432,5, 365,72, 499,72)
+      },
+
+      under: {
+        _0: two.makePolygon(792,432, 792,365, 499,72, 432,72, 432,432),
+        _1: two.makePolygon(432,72, 365,72, 72,365, 72,432, 432,432),
+        _2: two.makePolygon(792,499, 792,432, 432,432, 432,792, 499,792),
+        _3: two.makePolygon(72,432, 72,499, 365,792, 432,792, 432,432)
+      },
+
+      over: {
+        _0: two.makePolygon(365,365, 72,365, 365,72),
+        _1: two.makePolygon(499,365, 792,365, 499,72),
+        _2: two.makePolygon(365,499, 72,499, 365,792),
+        _3: two.makePolygon(499,792, 792,499, 499,499)
+      }
     },
-    under: {
-      _0: two.makePolygon(792,432, 792,365, 499,72, 432,72, 432,432),
-      _1: two.makePolygon(432,72, 365,72, 72,365, 72,432, 432,432),
-      _2: two.makePolygon(792,499, 792,432, 432,432, 432,792, 499,792),
-      _3: two.makePolygon(72,432, 72,499, 365,792, 432,792, 432,432)
+
+    colors: {
+      _0: pusher.color("red"),
+      _1: pusher.color("green")
     },
-    over: {
-      _0: two.makePolygon(365,365, 72,365, 365,72),
-      _1: two.makePolygon(499,365, 792,365, 499,72),
-      _2: two.makePolygon(365,499, 72,499, 365,792),
-      _3: two.makePolygon(499,792, 792,499, 499,499)
+
+    update: function() {
+      fillPair(pairs[0], this.colors._0.html());
+      fillPair(pairs[1], this.colors._0.complement().html());
+      fillPair(pairs[2], this.colors._1.html());
+      fillPair(pairs[3], this.colors._1.complement().html());
+
+      two.update();
     }
   }
 
-  var groups = Object.keys(Drawing).map(function(section) {
-    Drawing[section].group =
-      two.makeGroup(Drawing[section]._0, Drawing[section]._1, Drawing[section]._2, Drawing[section]._3);
+  var groups = Object.keys(Drawing.groups).map(function(section) {
+    Drawing.groups[section].group =
+      two.makeGroup(
+        Drawing.groups[section]._0,
+        Drawing.groups[section]._1,
+        Drawing.groups[section]._2,
+        Drawing.groups[section]._3
+      );
+
+    Drawing.groups[section].group.noStroke();
   });
 
-  Drawing.base.group.fill = "#000";
+  Drawing.groups.base.group.fill = "#eee";
 
   var pairs = [0, 0, 0, 0].map(function(_, i) {
-    return [Drawing.under["_" + i], Drawing.over["_" + i]]
+    return [Drawing.groups.under["_" + i], Drawing.groups.over["_" + i]]
   });
 
   var fillPair = function(pair, color) {
     pair.map(function(x) { x.fill = color; });
   }
 
-  fillPair(pairs[0], pusher.color("red").html());
-  fillPair(pairs[1], pusher.color("red").complement().html());
-  fillPair(pairs[2], pusher.color("green").html());
-  fillPair(pairs[3], pusher.color("green").complement().html());
+  Drawing.update();
 
-  two.update();
+  $(".color").on("change", function() {
+    var _color = $(this).data("color");
+    console.log(_color);
+    Drawing.colors[_color] = pusher.color(this.value);
+    Drawing.update();
+  });
 });
