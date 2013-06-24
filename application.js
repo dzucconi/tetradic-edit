7325d2006e764bed6799c1b07d03b3304603df9f
dzucconi <damonzucconi@gmail.com>
2013-06-24 14:13:34 -0400

    Rough interface

--- application.js
+++ application.js
@@ -15,70 +15,86 @@ $(function() {
   window.Drawing = {
     groups: {
       base: {
-        _0: two.makePolygon(5,432, 72,499, 72,365),
-        _1: two.makePolygon(859,432, 792,365, 792,499),
-        _2: two.makePolygon(432,859, 499,792, 365,792),
-        _3: two.makePolygon(432,5, 365,72, 499,72)
+        _0: two.makePolygon(792,365, 499,72, 432,5, 365,72, 72,365, 5,432, 72,499, 365,792, 432,859, 499,792, 792,499, 859,432)
       },
 
-      under: {
-        _0: two.makePolygon(792,432, 792,365, 499,72, 432,72, 432,432),
-        _1: two.makePolygon(432,72, 365,72, 72,365, 72,432, 432,432),
-        _2: two.makePolygon(792,499, 792,432, 432,432, 432,792, 499,792),
-        _3: two.makePolygon(72,432, 72,499, 365,792, 432,792, 432,432)
-      },
+      paper: {
+        under: {
+          _0: two.makePolygon(792,432, 792,365, 499,72, 432,72, 432,432),
+          _1: two.makePolygon(432,72, 365,72, 72,365, 72,432, 432,432),
+          _2: two.makePolygon(792,499, 792,432, 432,432, 432,792, 499,792),
+          _3: two.makePolygon(72,432, 72,499, 365,792, 432,792, 432,432)
+        },
 
-      over: {
-        _0: two.makePolygon(365,365, 72,365, 365,72),
-        _1: two.makePolygon(499,365, 792,365, 499,72),
-        _2: two.makePolygon(365,499, 72,499, 365,792),
-        _3: two.makePolygon(499,792, 792,499, 499,499)
+        over: {
+          _0: two.makePolygon(365,365, 72,365, 365,72),
+          _1: two.makePolygon(499,365, 792,365, 499,72),
+          _2: two.makePolygon(365,499, 72,499, 365,792),
+          _3: two.makePolygon(499,792, 792,499, 499,499)
+        }
       }
     },
 
     colors: {
       _0: pusher.color("red"),
-      _1: pusher.color("green")
+      _1: pusher.color("green"),
+      linked: false
     },
 
-    update: function() {
-      fillPair(pairs[0], this.colors._0.html());
-      fillPair(pairs[1], this.colors._0.complement().html());
-      fillPair(pairs[2], this.colors._1.html());
-      fillPair(pairs[3], this.colors._1.complement().html());
+    fillPair: function(pair, color) {
+      pair[0].fill = color.add("#090909").html();
+      pair[1].fill = color.html();
+    },
+
+    update: function(linked) {
+      if (this.colors.linked) { this.colors._1 = this.colors._0.hue("+90"); }
+
+      $("#c1")[0].value = this.colors._0.hex6();
+      $("#c2")[0].value = this.colors._1.hex6();
+
+      this.fillPair(pairs[0], this.colors._0);
+      this.fillPair(pairs[1], this.colors._0.complement());
+      this.fillPair(pairs[2], this.colors._1);
+      this.fillPair(pairs[3], this.colors._1.complement());
 
       two.update();
     }
   }
 
-  var groups = Object.keys(Drawing.groups).map(function(section) {
-    Drawing.groups[section].group =
+  var groups = Object.keys(Drawing.groups.paper).map(function(section) {
+    Drawing.groups.paper[section].group =
       two.makeGroup(
-        Drawing.groups[section]._0,
-        Drawing.groups[section]._1,
-        Drawing.groups[section]._2,
-        Drawing.groups[section]._3
+        Drawing.groups.paper[section]._0,
+        Drawing.groups.paper[section]._1,
+        Drawing.groups.paper[section]._2,
+        Drawing.groups.paper[section]._3
       );
 
-    Drawing.groups[section].group.noStroke();
+    Drawing.groups.paper[section].group.noStroke();
   });
 
-  Drawing.groups.base.group.fill = "#eee";
+  Drawing.groups.base.stroke = "#777";
 
   var pairs = [0, 0, 0, 0].map(function(_, i) {
-    return [Drawing.groups.under["_" + i], Drawing.groups.over["_" + i]]
+    return [Drawing.groups.paper.under["_" + i], Drawing.groups.paper.over["_" + i]]
   });
-
-  var fillPair = function(pair, color) {
-    pair.map(function(x) { x.fill = color; });
-  }
 
   Drawing.update();
 
   $(".color").on("change", function() {
     var _color = $(this).data("color");
-    console.log(_color);
+
     Drawing.colors[_color] = pusher.color(this.value);
     Drawing.update();
+  });
+
+  $("#linked").on("click", function(e) {
+    e.preventDefault();
+
+    var $el = $(this);
+
+    Drawing.colors.linked = !$el.data("value");
+    $el.data("value", Drawing.colors.linked);
+    $el.text($el.data(Drawing.colors.linked.toString()));
   });
 });
